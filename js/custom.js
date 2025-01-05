5684c653c4a7a9fd020bf9176cd56bfb5a75c4bb
Swapnil <team@mobeserv.com>
2025-01-05 15:55:35 +0530

    Ios Native false

--- js/custom.js
+++ js/custom.js
@@ -116,39 +116,16 @@ function playYoututbeVideo(){
 }
 
 document.addEventListener('DOMContentLoaded', () => { 
-    // This is the bare minimum JavaScript. You can opt to pass no arguments to setup.
-    const player = new Plyr('#bannervideo');
-    
-    // Expose
-    window.player = player;
-  
-    // Bind event listener
-    function on(selector, type, callback) {
-      document.querySelector(selector).addEventListener(type, callback, false);
-    }
-  
-    // Play
-    on('.js-play', 'click', () => { 
-      player.play();
-    });
-  
-    // Pause
-    on('.js-pause', 'click', () => { 
-      player.pause();
-    });
-  
-    // Stop
-    on('.js-stop', 'click', () => { 
-      player.stop();
-    });
-  
-    // Rewind
-    on('.js-rewind', 'click', () => { 
-      player.rewind();
-    });
-  
-    // Forward
-    on('.js-forward', 'click', () => { 
-      player.forward();
-    });
+    const options = {
+        fullscreen: {
+          enabled: true,
+          iosNative: false
+        },
+        controls: [
+          
+        ],
+        muted: true,
+        storage: { enabled: false }
+      };
+      const player = new Plyr(document.querySelector("#bannervideo"), options);      
   });
